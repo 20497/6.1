a35fe5058f207193e576fb2068b2c72811ef9c3d
20497 <s20497@pjwstk.edu.pl>
2022-06-12 16:38:21 +0200

    6.1

---
+++ src/displayData.js
@@ -0,0 +1,16 @@
+export default function DisplayData({data}) {
+    return (
+        <>
+            <h1 className="title">RANDOM USER GENERATOR</h1>
+            <div className="user-profile">
+                <img id="img" src={data.results[0].picture.large}/>
+                <div id="name">Full name: {data.results[0].name.first + " " + data.results[0].name.last}</div>
+                <div className="description">
+                    <div>Username: <span id="username">{data.results[0].login.username}</span></div>
+                    <div>Email: <span id="email">{data.results[0].email}</span></div>
+                    <div>City: <span id="city">{data.results[0].location.city}</span></div>
+                </div>
+            </div>
+        </>
+    );
+}
